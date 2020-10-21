3257f734079bd8f0c6bfaed01cfec4b5ded6b9dd
Eduardo Keneeth <eduardokeneeth@gmail.com>
2020-10-21 10:50:25 -0300

    feat(constants): adding endpoints

--- src/constants.js
+++ src/constants.js
@@ -2,7 +2,12 @@ exports.CONSTANTS = {
     ENVS: ['TEST', 'STAGE', 'PROD', 'NOENV'],
     PATHS: {
         SRC: './src',
+        SSE: './sse',
         DCU: './DesignCodeUtility/dcuIndex.js',
-        CCW: './DesignCodeUtility/ccwIndex.js'
+        CCW: './DesignCodeUtility/ccwIndex.js',
     },
+    ENDPOINT: {
+        LOGIN: '/login',
+        SSE_LIST: '/serverExtensions',
+    }
 }
