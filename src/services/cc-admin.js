f9d384ef3bcd610a0c7fec2556ef2cd7e6e297df
Eduardo Keneeth <eduardokeneeth@gmail.com>
2020-10-21 10:44:40 -0300

    feat(ccadmin): adding api interface for each env

--- src/services/cc-admin.js
+++ src/services/cc-admin.js
@@ -1,8 +1,25 @@
 require('dotenv').config();
 const axios = require('axios');
 
-const api = axios.create({
-    baseURL: `${process.env.OCC_ADMIN_URL}/ccadmin/v1`,
+const TEST = axios.create({
+    baseURL: `${process.env.OCC_TEST_ADMIN_URL}/ccadmin/v1`,
 });
 
-exports.ccAdminApi = api;
+const STAGE = axios.create({
+    baseURL: `${process.env.OCC_STAGE_ADMIN_URL}/ccadmin/v1`,
+});
+
+const PROD = axios.create({
+    baseURL: `${process.env.OCC_PROD_ADMIN_URL}/ccadmin/v1`,
+});
+
+const NOENV = axios.create({
+    baseURL: `${process.env.OCC_NOENV_ADMIN_URL}/ccadmin/v1`,
+});
+
+exports.ccAdminApi = {
+    TEST,
+    STAGE,
+    PROD,
+    NOENV,
+};
