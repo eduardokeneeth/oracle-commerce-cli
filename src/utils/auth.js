0d9a4f43ff1afa4aae0e8f5100e6464e5ffbaaea
Eduardo Keneeth <eduardokeneeth@gmail.com>
2020-10-21 10:42:54 -0300

    feat(auth): adding multiple environments auth option

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -4,7 +4,9 @@ const { ccAdminApi } = require('./../services/cc-admin');
 const { CONSTANTS } = require('./../constants');
 
 const Methods = {
-    login: async () => {
+    login: async (env) => {
+        env = env || process.env.ACTIVE_ENV;
+
         const data = qs.stringify({
             'grant_type': 'client_credentials' 
         });
@@ -12,14 +14,17 @@ const Methods = {
         const config = {
             headers: {
                 ContentType: 'application/x-www-form-urlencoded',
-                Authorization: `Bearer ${process.env.OCC_APP_KEY}`,
+                Authorization: `Bearer ${process.env[`OCC_${env}_APP_KEY`]}`,
             },
         };
 
-        const response = await ccAdminApi.post(CONSTANTS.ENDPOINT.LOGIN, data, config);
-        const { access_token } = response.data;
-        
-        return access_token;
+        try {
+            const response = await ccAdminApi[env].post(CONSTANTS.ENDPOINT.LOGIN, data, config);
+            const { access_token } = response.data;
+            return access_token;
+        } catch (e) {
+            console.log(`Not authorized to access ${env}. Please review your environment configuration.`);
+        }
     },
 };
 
